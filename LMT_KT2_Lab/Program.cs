4fb5f342a489b96478853f8709f78dbb54466a89
Minh <conkhi3695@gmail.com>
2025-07-25 08:16:25 +0700

    Lab2_KT

---
+++ LMT_KT2_Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMT_KT2_Lab
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new F_DangNhap());
+        }
+    }
+}
